3341ef634789f5b3bef933ca0badf7ccf951e676
Laiza Costa <laizaa_oliveira@yahoo.com.br>
2022-07-18 21:40:18 -0300

    ok

---
+++ laizaFlix.js
@@ -0,0 +1,10 @@
+var listafilmes = [
+    "https://media.fstatic.com/-mjmlX1cEXsuiWsBOgDRYMZqz80=/290x478/smart/media/movies/covers/2018/01/7VTALkqjG40vby3uVIsp03d7yXy.jpg",
+    "https://filosofiaeocinema.files.wordpress.com/2016/08/20224832.jpg",
+    "https://static.wixstatic.com/media/136b2c_7440a2bb9d4c4e9f9ae531b944ed492c~mv2.jpg/v1/fit/w_1000%2Ch_675%2Cal_c%2Cq_80/file.jpg"
+  ];
+  
+  for (var i = 0; i < listafilmes.length; i++) {
+    document.write("<img src=" + listafilmes[i] + ">");
+  }
+  
